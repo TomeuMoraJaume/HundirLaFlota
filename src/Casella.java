03566002e80b531e0f89f64567580524f87239c1
TomeuMoraJaume <tomeumorajaume05@gmail.com>
2025-03-18 11:52:33 +0100

    Creacio basica casella

--- src/Casella.java
+++ src/Casella.java
@@ -1,12 +1,17 @@
 public class Casella {
-    private char simbol;
 
+    private final char simbol;
+    private boolean tepesaBarco;
+    private boolean destapada;
     public Casella(char simbol) {
         this.simbol = simbol;
+        this.destapada = false;
     }
 
     @Override
     public String toString() {
         return String.valueOf(simbol);
     }
+
+
 }
